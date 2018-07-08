f29089157e879092326b320d09e0bac10fdffd8d
Kyle Alzayadi <kylealzayadi@kyles-mbp.lan>
2018-07-07 17:10:14 -0700

    finsihed navbar and 3 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { homedir } from 'os';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:"Home",
+  component:HomeComponent},
+   
+    {path:"Contacts",
+    component:ContactsComponent},
+    {path:"About",
+    component:AboutComponent},
+    
+      ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
